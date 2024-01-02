102bafc1cd16b29d6705f2eb8493bc06e6db13c8
krivvv <124560164+krivvv@users.noreply.github.com>
2024-01-02 19:02:23 +0300

    +lab5

---
+++ lab_5/hignt/hignt.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <string.h>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+struct Array
+{
+	int n;
+	char arr[51];
+};
+
+int main(int argc, char* argv[]) {
+	HANDLE hReadChannelReady = OpenEvent(EVENT_ALL_ACCESS, FALSE, (LPCSTR)"WRITE_CHANNEL_READY");
+	HANDLE hWriteChannelReady = OpenEvent(EVENT_ALL_ACCESS, FALSE, (LPCSTR)"READ_CHANNEL_READY");
+	int dwBytesWritten = 0;
+	int n;
+	HANDLE hWritePipe = (HANDLE)atoi(argv[1]),	hReadPipe = (HANDLE)atoi(argv[0]);
+	WaitForSingleObject(hReadChannelReady, INFINITE);
+	if (!ReadFile(hReadPipe, (char*)&n, sizeof(int), (LPDWORD)&dwBytesWritten, NULL))
+	{
+		_cputs("Read from file failed.\n");
+		_cputs("Press any key to finish.\n");
+		_getch();
+		return GetLastError();
+	}
+	vector <_int16> b(n);
+	srand(static_cast<unsigned int>(time(0)));
+	cout << "Elements of array: ";
+	for (int i = 0; i < n; ++i) {
+		b[i] = rand() % 100; 
+		cout << b[i] << ' ';
+	}
+	cout << endl;
+	int n1;
+	cout << " Enter N: ";
+	cin >> n1;
+	vector<_int16> a;
+	cout << "Elements that are greater than N: " << endl;
+	for (_int16 i = 0; i < b.size(); i++) {
+		if (b[i] > n1) {
+			a.push_back(b[i]);
+			cout << b[i] << ' ';
+		}
+	}
+	int size = a.size();
+	if (!WriteFile(hWritePipe, &size, sizeof(int), (LPDWORD)&dwBytesWritten, NULL))
+	{
+		_cputs("Write to file failed.\n");
+		_cputs("Press any key to finish.\n");
+		_getch();
+		return GetLastError();
+	}
+	SetEvent(hWriteChannelReady);
+	for (_int16 i = 0; i < size; ++i)
+	{
+		WaitForSingleObject(hReadChannelReady, INFINITE);
+		if (!WriteFile(hWritePipe, &(a[i]), sizeof(a[i]), (LPDWORD)&dwBytesWritten, NULL))
+		{
+			_cputs("Write to file failed.\n");
+			_cputs("Press any key to finish.\n");
+			_getch();
+			return GetLastError();
+		}
+		SetEvent(hWriteChannelReady);
+
+	}
+	cout << endl;
+	_cputs("Press any key to finish.\n");
+	_getch();
+	return 0;
+}
---
+++ lab_5/server/server.cpp
@@ -0,0 +1,120 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <time.h>
+#include <vector>
+using namespace std;
+
+int main() {
+    HANDLE hWriteChannelReady = CreateEvent(NULL, FALSE, FALSE, (LPCSTR)"WRITE_CHANNEL_READY");
+    HANDLE hReadChannelReady = CreateEvent(NULL, FALSE, FALSE, (LPCSTR)"READ_CHANNEL_READY");
+    HANDLE hPipeRead, hPipeWrite, hReadPipe, hWritePipe;
+    SECURITY_ATTRIBUTES sa;
+    sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+    sa.lpSecurityDescriptor = NULL;
+    sa.bInheritHandle = FALSE;
+    int n;
+    cout << "Enter array size: " << endl;
+    cin >> n;
+    if (!CreatePipe(&hPipeRead, &hPipeWrite, &sa, 0))
+    {
+        _cputs("Create pipe failed.\n");
+        _cputs("Press any key to finish.\n");
+        _getch();
+        return GetLastError();
+    }
+    if (!DuplicateHandle(
+        GetCurrentProcess(), 
+        hPipeWrite, 
+        GetCurrentProcess(), 
+        &hWritePipe, 
+        0,
+        TRUE, 
+        DUPLICATE_SAME_ACCESS))
+    {
+        _cputs("Duplicate handle failed.\n");
+        _cputs("Press any key to finish.\n");
+        _getch();
+        return GetLastError();
+    }
+    CloseHandle(hPipeWrite);
+    if (!DuplicateHandle(
+        GetCurrentProcess(), 
+        hPipeRead, 
+        GetCurrentProcess(), 
+        &hReadPipe, 
+        0, 
+        TRUE,
+        DUPLICATE_SAME_ACCESS))
+    {
+        _cputs("Duplicate handle failed.\n");
+        _cputs("Press any key to finish.\n");
+        _getch();
+        return GetLastError();
+    }
+    CloseHandle(hPipeRead);
+    STARTUPINFO si;
+    PROCESS_INFORMATION piApp;
+
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+    char lpszAppName[] = "C:\\Users\\ro\\source\\repos\\server\\x64\\Debug\\hignt.exe";
+    char lpszParameter[20];
+    sprintf_s(lpszParameter, "%d %d", (int)hReadPipe, (int)hWritePipe);
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    if (!CreateProcessA((LPCSTR)lpszAppName, lpszParameter, nullptr, nullptr, TRUE,
+        CREATE_NEW_CONSOLE, nullptr, nullptr, (LPSTARTUPINFOA)&si, &piApp))
+    {
+        _cputs("The new process is not created.\n");
+        _cputs("Check a name of the process.\n");
+        _cputs("Press any key to finish.\n");
+        _getch();
+        return 0;
+    }
+    int dwBytesWritten = 0;
+    if (!WriteFile(hWritePipe, (char*)&n, sizeof(int), (LPDWORD)&dwBytesWritten, NULL))
+    {
+        _cputs("Write to file failed.\n");
+        _cputs("Press any key to finish.\n");
+        _getch();
+        return GetLastError();
+    }
+    SetEvent(hWriteChannelReady);
+    WaitForSingleObject(hReadChannelReady, INFINITE);
+    int n1;
+    if (!ReadFile(hReadPipe, &(n1), sizeof(int), (LPDWORD)&dwBytesWritten, NULL))
+    {
+        _cputs("Read from file failed.\n");
+        _cputs("Press any key to finish.\n");
+        _getch();
+        return GetLastError();
+    }
+    SetEvent(hWriteChannelReady);
+    vector<_int16> a(n1);
+    for (int i = 0; i < n1; ++i)
+    {
+        WaitForSingleObject(hReadChannelReady, INFINITE);
+        if (!ReadFile(hReadPipe, &(a[i]), sizeof(a[i]), (LPDWORD)&dwBytesWritten, NULL))
+        {
+            _cputs("Read from file failed.\n");
+            _cputs("Press any key to finish.\n");
+            _getch();
+            return GetLastError();
+        }
+        SetEvent(hWriteChannelReady);
+    }
+    for (int i = 0; i < n1; i++) {
+        cout << a[i] << ' ';
+    }
+    cout << endl;
+    _cputs("Press any key to finish.\n");
+    _getch();
+    CloseHandle(hWriteChannelReady);
+    CloseHandle(hReadChannelReady);
+    CloseHandle(piApp.hThread);
+    CloseHandle(piApp.hProcess);
+    CloseHandle(hReadPipe);
+    CloseHandle(hWritePipe);
+
+    return 0;
+}
